8d05f10bda17540d01e2334f5bf0d07090911dcf
Marcelo Mangelli <marcelomangelliv@gmail.com>
2026-06-10 19:55:30 -0300

    adiciona botao de login a tela home

--- js/home.js
+++ js/home.js
@@ -2,11 +2,19 @@ firebase.auth().onAuthStateChanged((user) => {
   document.body.style.visibility = "visible";
   const logoutBtn = document.getElementById("logout-button");
   const logoutBtnMobile = document.getElementById("logout-button-mobile");
+  const loginBtn = document.getElementById("login-button");
+  const loginBtnMobile = document.getElementById("login-button-mobile");
   if (logoutBtn) {
     logoutBtn.style.display = user ? "block" : "none";
   }
   if (logoutBtnMobile) {
     logoutBtnMobile.style.display = user ? "block" : "none";
+  }
+  if (loginBtn) {
+    loginBtn.style.display = user ? "none" : "block";
+  }
+  if (loginBtnMobile) {
+    loginBtnMobile.style.display = user ? "none" : "block";
   }
   const ctaBtn = document.getElementById("cta-button");
   if (ctaBtn && user) {
